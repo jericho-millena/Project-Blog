1e444bd132136e9710a9013ef8f96cdcf24c125a
jericho-millena <21-03025@g.batstate-u.edu.ph>
2024-07-06 19:17:14 +0800

    add dotenv

--- client/src/assets/firebase/firebaseInit.js
+++ client/src/assets/firebase/firebaseInit.js
@@ -2,12 +2,12 @@ import firebase from 'firebase/app'
 import 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDt4GKeBiUUm0P5huxelUFVDbjhKVPJFJM',
-  authDomain: 'myblog-23b89.firebaseapp.com',
-  projectId: 'myblog-23b89',
-  storageBucket: 'myblog-23b89.appspot.com',
-  messagingSenderId: '240753796263',
-  appId: '1:240753796263:web:7bfd258d4c74d64083e782'
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGINGSENDER_ID,
+  appId: process.env.APP_ID
 }
 
 const firebaseApp = firebase.initializeApp(firebaseConfig)
